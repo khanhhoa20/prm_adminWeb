caaeae04150b5cc542ba2b32cde7df7cd182c744
khanhhoa <khanhhoa12t2@gmail.com>
2021-10-25 23:27:24 +0700

    cách chỉnh màu trong sidebar xem useSidebar.js

--- share/js/useSidebar.js
+++ share/js/useSidebar.js
@@ -13,8 +13,29 @@ fetch('share/dashboard.html')
 
         // You can now even select part of that html as you would in the regular DOM 
         // Example:
-        var sidebar = doc.querySelector('.sidebar');     
+        var sidebar = doc.querySelector('.sidebar'); 
+        var thanhsearch = doc.querySelector('.thanhsearch')
         document.getElementById("sidebar").append(sidebar);
+        document.getElementById("thanhsearch").append(thanhsearch);
+
+//Cách chỉnh màu cam được chọn ở sidebar
+//xóa class active cũ đi  và
+// nếu url chứa ... thì thêm class active vào element có id là... 
+       //example
+        // console.log(window.location.href.includes("service")); 
+
+        //nếu url chứa service thì
+        // if (window.location.href.includes("service")){
+        //xóa class active ở sidebar service
+        //document.querySelector(".active").classList.remove("active");  
+        //(màu cam đc chọn mất)
+        //     
+        //thêm class active ở service     
+        //document.getElementById('chooseService').classList.add('active'); 
+        //màu cam chọn xuất hiện :v
+        // }
+
+        
         
         var script = document.createElement("script");  // create a script DOM node
         script.src = "js/auth.js";  // set its src to the provided URL
