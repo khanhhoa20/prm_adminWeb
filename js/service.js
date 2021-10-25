12a85845f8b536d4d4d18af476ff9b13987c79e5
khanhhoa <khanhhoa12t2@gmail.com>
2021-10-25 10:42:44 +0700

    login logout done

--- js/service.js
+++ js/service.js
@@ -1,36 +1,36 @@
 //giao diện phần sidebar
-fetch('share/dashboard.html')
-    .then(function(response) {
-        // When the page is loaded convert it to text
-        return response.text()
-    })
-    .then(function(html) {
-        // Initialize the DOM parser
-        var parser = new DOMParser();
+// fetch('share/dashboard.html')
+//     .then(function(response) {
+//         // When the page is loaded convert it to text
+//         return response.text()
+//     })
+//     .then(function(html) {
+//         // Initialize the DOM parser
+//         var parser = new DOMParser();
 
-        // Parse the text
-        var doc = parser.parseFromString(html, "text/html");
+//         // Parse the text
+//         var doc = parser.parseFromString(html, "text/html");
 
-        // You can now even select part of that html as you would in the regular DOM 
-        // Example:
-        var sidebar = doc.querySelector('.sidebar');
+//         // You can now even select part of that html as you would in the regular DOM 
+//         // Example:
+//         var sidebar = doc.querySelector('.sidebar');
 
       
-        document.getElementById("sidebar").append(sidebar);
-        var script = document.createElement("script");  // create a script DOM node
-        script.src = "js/auth.js";  // set its src to the provided URL
+//         document.getElementById("sidebar").append(sidebar);
+//         var script = document.createElement("script");  // create a script DOM node
+//         script.src = "js/auth.js";  // set its src to the provided URL
     
-        document.body.appendChild(script);  // add it to the end of the body section of the page (could change 'head' to 'body' to add it to the end of the body section instead)
+//         document.body.appendChild(script);  // add it to the end of the body section of the page (could change 'head' to 'body' to add it to the end of the body section instead)
 
 
 
-        console.log(document.querySelector(".logout"));
-        console.log(sidebar);
+//         console.log(document.querySelector(".logout"));
+//         console.log(sidebar);
 
         
         
-    })
-    .catch(function(err) {  
-        console.log('Failed to fetch page: ', err);  
-    });
+//     })
+//     .catch(function(err) {  
+//         console.log('Failed to fetch page: ', err);  
+//     });
 
--- share/js/useSidebar.js
+++ share/js/useSidebar.js
@@ -1,3 +1,4 @@
+//giao diện phần sidebar
 fetch('share/dashboard.html')
     .then(function(response) {
         // When the page is loaded convert it to text
@@ -12,14 +13,23 @@ fetch('share/dashboard.html')
 
         // You can now even select part of that html as you would in the regular DOM 
         // Example:
-        var sidebar = doc.querySelector('.sidebar');
-
+        var sidebar = doc.querySelector('.sidebar');     
         document.getElementById("sidebar").append(sidebar);
         
+        var script = document.createElement("script");  // create a script DOM node
+        script.src = "js/auth.js";  // set its src to the provided URL
+    
+        document.body.appendChild(script);  // add it to the end of the body section of the page (could change 'head' to 'body' to add it to the end of the body section instead)
+
+
+
+        console.log(document.querySelector(".logout"));
         console.log(sidebar);
 
-        console.log(doc);
+        
+        
     })
     .catch(function(err) {  
         console.log('Failed to fetch page: ', err);  
     });
+
