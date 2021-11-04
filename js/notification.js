dfa4884eb57f138761c4706bc2c9cf519d252a77
truong0145 <66652065+truong0145@users.noreply.github.com>
2021-11-04 20:37:15 +0700

    update navbar

--- js/notification.js
+++ js/notification.js
@@ -7,39 +7,48 @@ function getOnProcessAppointmentNotifiView(_data) {
     var noti_number = document.getElementById('notifi-number-140562');
     var count = 0;
 
-    if(noti_content.style.display == 'none'){
+    if (noti_content.style.display == 'none') {
         for (var i = _data.length - 1; i >= 0; i--) {
             if (_data[i].status == 'ON PROCESS') {
                 count++;
             }
         }
-    
-        noti_number.innerHTML = count;
-        noti_number.style.display = 'inline-block';
+
+        if (count > 0) {
+            noti_number.innerHTML = count;
+            noti_number.style.display = 'inline-block';
+        }
     }
 }
 
 function getOnProcessAppointmentNotifiContentView(_data) {
     var noti_content = document.getElementById('noti-content-140562');
-
+    var isOnProcess = false;
     noti_content.innerHTML = '';
 
-        for (var i = _data.length - 1; i >= 0; i--) {
-            if (_data[i].status == 'ON PROCESS') {
-                noti_content.innerHTML+= `<div class="p-2 m-3 border-bottom"><a class="text-dark text-decoration-none" href="appointment-arrange.html?id=${_data[i].apptID}">New appointment. Id ${_data[i].apptID}</a></div>`;
-            }
+    for (var i = _data.length - 1; i >= 0; i--) {
+        if (_data[i].status == 'ON PROCESS') {
+            noti_content.innerHTML += `<div class="p-2 m-3 border-bottom"><a class="text-dark text-decoration-none" href="appointment-arrange.html?id=${_data[i].apptID}">New appointment. Id ${_data[i].apptID}</a></div>`;
+            isOnProcess = true;
         }
-    
+    }
+    if (!isOnProcess) {
+        noti_content.innerHTML += '<div class="p-2 m-3 ">You don\'t have any upcoming appointment.</div>';
+    }
+
+
+
+
 }
 
-function changeNotiDisplay(){
+function changeNotiDisplay() {
     var noti_content = document.getElementById('noti-content-140562');
     var noti_number = document.getElementById('notifi-number-140562');
 
-    if(noti_content.style.display == 'none'){
+    if (noti_content.style.display == 'none') {
         noti_content.style.display = 'inline-block';
         noti_number.style.display = 'none';
-    }else{
+    } else {
         noti_content.style.display = 'none';
     }
 }
