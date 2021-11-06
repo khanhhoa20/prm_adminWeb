2bac2a7dd54d48de26e298168760af8b75e21eba
truong0145 <66652065+truong0145@users.noreply.github.com>
2021-11-06 22:22:24 +0700

    add list removed employee, restore employee

--- js/employee/employee.js
+++ js/employee/employee.js
@@ -16,8 +16,9 @@ async function getEmployeesAndPaging() {
 
         }
     );
-    var dataEmployee = await x.json();
-    // console.log(dataEmployee);
+    var dataEmployee = [];
+    x.status == 200 ? dataEmployee = await x.json() : '';
+
     $('#hello').pagination({
         dataSource: dataEmployee,
         pageSize: 20,
@@ -32,6 +33,59 @@ async function getEmployeesAndPaging() {
     })
 
 
+}
+
+async function getRemovedEmployeesAndPaging() {
+    const formData = new FormData();
+    formData.append('roleID', 'st');
+    var x = await fetch('https://hair-cut.herokuapp.com/api/getEmployeeStatusFalse',
+
+        {
+            method: "post",
+            headers: {
+
+                Authorization: sessionStorage.getItem('token')
+
+            },
+            body: formData
+
+
+        }
+    );
+    var dataEmployee = [];
+    x.status == 200 ? dataEmployee = await x.json() : '';
+
+    $('#hello').pagination({
+        dataSource: dataEmployee,
+        pageSize: 20,
+        showGoInput: true,
+        showGoButton: true,
+        showPrevious: false,
+        showNext: false,
+        callback: function (data, pagination) {
+            var html = renderEmployees(data);
+            $('#tableEmployees').html(html);
+        }
+    })
+
+
+}
+
+function changeEmployeesViews() {
+    var _employee_show = document.getElementById('employee-show');
+    var _employee_list_title = document.getElementById('employee-list-title');
+
+    if (_employee_show.classList.contains('text-danger')) {
+        _employee_show.classList.replace('text-danger', 'text-success');
+        _employee_show.innerHTML = '<i class="fas fa-bars"></i>&ensp;Active employees';
+        _employee_list_title.innerHTML = 'Removed employee list';
+        getRemovedEmployeesAndPaging();
+    } else {
+        _employee_show.classList.replace('text-success', 'text-danger');
+        _employee_show.innerHTML = '<i class="fas fa-bars"></i>&ensp;Removed employees';
+        _employee_list_title.innerHTML = 'Available employee list';
+        getEmployeesAndPaging();
+    }
 }
 
 getEmployeesAndPaging();
@@ -63,9 +117,11 @@ function renderEmployees(employees) {
             <td>${employee.seatNum}</td>
             <td>${employee.scheduleID}</td>
 
-
-            <td><a href="/employeeUpdate.html?id=${employee.empEmail}">Update</a></td>
-            <td><button onclick="handleRemoveEmployee('${employee.empEmail}')" type="button" class="btn btn-danger">Remove</button></td>
+            <td><a class="${employee.status ? 'btn btn-primary' : 'btn btn-secondary disabled'}" href="/employeeUpdate.html?id=${employee.empEmail}">Update</a></td>
+            ${employee.status ?
+                `<td><button style="width: 6em;" onclick="handleRemoveEmployee('${employee.empEmail}')" type="button" class="btn btn-danger">Remove</button></td>` :
+                `<td><button style="width: 6em;" onclick="handleRestoreEmployee('${employee.empEmail}')" type="button" class="btn btn-success">Restore</button></td>`
+            }
         </tr> 
         `
 
@@ -129,6 +185,48 @@ function handleRemoveEmployee(empEmail) {
 
 }
 
+function handleRestoreEmployee(empEmail) {
+    console.log(empEmail);
+    var bodyContent = document.querySelector('body').innerHTML;
+    if (bodyContent.includes("staticBackdropRestore")) {
+        $('#staticBackdropRestore').remove();
+    }
+    function restoreEmployee() {
+
+        let formData = new FormData();
+        formData.append('empEmail', empEmail);
+
+        fetch('https://hair-cut.herokuapp.com/api/restoreEmployeeByEmpEmail',
+
+            {
+                method: "put",
+                headers: {
+
+                    Authorization: sessionStorage.getItem('token'),
+
+
+                },
+                body: formData
+
+
+            }
+        )
+        .then(function(){
+            getRemovedEmployeesAndPaging()
+        })
+    }
+    createDialogRestore();
+    $('#staticBackdropRestore').modal('show');
+    $('#confirmRestore').click(function () {
+        restoreEmployee();
+        $('#staticBackdropRestore').modal('hide')
+
+
+    });
+
+
+}
+
 // createDialog() for removeService;
 function createDialog() {
     var myModal = `  <div class="modal fade" id="staticBackdrop" data-backdrop="static" data-keyboard="false" tabindex="-1"
@@ -146,6 +244,37 @@ function createDialog() {
             </div>
             <div class="modal-footer">
             <button type="button" class="btn btn-danger" id="confirmRemove">Yes</button>
+                <button type="button" class="btn btn-success" data-dismiss="modal" id="notRemove">No</button>
+                
+            </div>
+        </div>
+    </div>
+</div>`
+
+    $('body').append(myModal);
+
+    // $('#staticBackdrop').modal('show');
+
+
+}
+
+
+function createDialogRestore() {
+    var myModal = `  <div class="modal fade" id="staticBackdropRestore" data-backdrop="static" data-keyboard="false" tabindex="-1"
+    aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="staticBackdropLabel">Message</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                Do you want to restore this employee?
+            </div>
+            <div class="modal-footer">
+            <button type="button" class="btn btn-danger" id="confirmRestore">Yes</button>
                 <button type="button" class="btn btn-success" data-dismiss="modal" id="notRemove">No</button>
                 
             </div>
